4ce4115a19fc0b03e0732b73710d9d225b3d5346
bosaniom <monika.bosaniova@gmail.com>
2023-03-31 00:48:43 +0200

    spiral mechanics update, gates added

--- Assets/Scripts/Mechanics/Pirouette/BallInSpiralController.cs
+++ Assets/Scripts/Mechanics/Pirouette/BallInSpiralController.cs
@@ -7,13 +7,11 @@ public class BallInSpiralController : MonoBehaviour
 {
     private Vector2 moveVector = Vector2.zero;
     private Rigidbody2D sphereRigidBody = null;
-    MechanicsControls mechanicsConstrolsActions = null;
     float speed = 50f;
 
     private void Awake()
     {
         sphereRigidBody = GetComponent<Rigidbody2D>();
-        mechanicsConstrolsActions = new MechanicsControls();
 
 
 
@@ -21,31 +19,20 @@ public class BallInSpiralController : MonoBehaviour
 
     private void OnEnable()
     {
-        mechanicsConstrolsActions.Enable();
-        mechanicsConstrolsActions.Pirouette.Move.performed += Move_performed;
-        mechanicsConstrolsActions.Pirouette.Move.canceled += Move_canceled;
 
     }
 
     private void OnDisable()
     {
-        mechanicsConstrolsActions.Enable();
-        mechanicsConstrolsActions.Pirouette.Move.performed -= Move_performed;
-        mechanicsConstrolsActions.Pirouette.Move.canceled -= Move_canceled;
     }
 
     private void FixedUpdate()
     {
         sphereRigidBody.velocity = moveVector * speed;
     }
-    private void Move_performed(InputAction.CallbackContext context)
+    public void Move(InputAction.CallbackContext context)
     {
         moveVector = context.ReadValue<Vector2>();
-
-    }
-    private void Move_canceled(InputAction.CallbackContext context)
-    {
-        moveVector = Vector2.zero;
 
     }
 }
