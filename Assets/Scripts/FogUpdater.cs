3e86f3cb4f2494d5ad0cfbd5b6d6ea6917facaca
David Napravnik <d@nogare.cz>
2023-03-30 02:17:10 +0200

    added river, obsticles, rocks and sun beams

--- Assets/Scripts/FogUpdater.cs
+++ Assets/Scripts/FogUpdater.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
+[ExecuteInEditMode]
 public class FogUpdater : MonoBehaviour
 {   
-public Material material;
+    public Material material;
+    public ScriptableRendererFeature scriptableRendererFeature;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (Application.isPlaying)
+            scriptableRendererFeature.SetActive(true);
+        else
+            scriptableRendererFeature.SetActive(false);
     }
 
     // Update is called once per frame
