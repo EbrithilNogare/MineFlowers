c871d6b5d3ffb56e684f6d9487fb6eb154645022
Jitka Uhříčková <uhrickova.jitka@seznam.cz>
2023-04-25 18:57:12 +0200

    spin-minigame

---
+++ Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    Animator animator;
+    public spiralMinigameHandler minigame;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (minigame.minigameOn)
+        {
+            animator.SetBool("SpiralMinigameOn", true);
+            animator.SetFloat("SpinSpeed", minigame.spinSpeed);
+        }
+    }
+}
