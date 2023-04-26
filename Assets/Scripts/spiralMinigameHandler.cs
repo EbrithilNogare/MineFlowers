ed92e69cf66aaabca912e79de0477827408d564e
David Napravnik <d@nogare.cz>
2023-04-26 21:59:42 +0200

    spin-minigame Co-authored-by: Jitka Uhříčková <uhrickova.jitka@seznam.cz>

--- Assets/Scripts/spiralMinigameHandler.cs
+++ Assets/Scripts/spiralMinigameHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,19 +8,35 @@ public class spiralMinigameHandler : MonoBehaviour
     public GameObject playerBall;
     public GameObject enemyBall;
     public GameObject gates;
+    public GameObject finish;
+
+    public float enemyDistanceToFin;
+    public float playerDistanceToFin;
+    public float distanceDifference;
+    public float spinSpeed;
 
     private Vector3 resetStartPositionPlayer;
     private Vector3 resetStartPositionEnemy;
 
+    public bool minigameOn;
     private void Start()
     {
         resetStartPositionPlayer = playerBall.transform.localPosition;
         resetStartPositionEnemy = enemyBall.transform.localPosition;
     }
 
+    private void Update()
+    {
+        enemyDistanceToFin = Vector3.Distance(enemyBall.transform.position, finish.transform.position);
+        playerDistanceToFin = Vector3.Distance(playerBall.transform.position, finish.transform.position);
+        distanceDifference = enemyDistanceToFin - playerDistanceToFin;
+        spinSpeed = Mathf.Lerp(0.3f, 2f, distanceDifference / 150);
+    }
+
     private void OnEnable()
     {
         Debug.Log("Enable!!!");
+        minigameOn = true;
         for (int i = 0; i < gates.transform.childCount; i++)
         {
             gates.transform.GetChild(i).gameObject.SetActive(true);
