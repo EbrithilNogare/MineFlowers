6e73c3fc1e2fda7c37a668e537711f92d242bdf7
Monika Bošániová <monika.bosaniova@gmail.com>
2023-05-30 18:07:44 +0200

    feature: balance mechanics

--- Assets/Scripts/Mechanics/Balance/BalanceMechanics.cs
+++ Assets/Scripts/Mechanics/Balance/BalanceMechanics.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class BalanceMechanics : MonoBehaviour
 {
