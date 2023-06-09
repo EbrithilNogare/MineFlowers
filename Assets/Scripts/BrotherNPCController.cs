792132b0e5c24f6129bd57de89a4a0e89abb8ac4
David Napravnik <d@nogare.cz>
2023-06-09 13:25:53 +0200

    cleaned scripts and added credits screen

---
+++ Assets/Scripts/BrotherNPCController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BrotherNPCController : MonoBehaviour
+{
+
+    public GameObject FlowersController;
+    public AudioClip GoodEndingAudio;
+    public AudioClip BadEndingAudio;
+
+    public int CountTreshold;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        int count = 0;
+        int.TryParse(FlowersController.GetComponent<FlowerCounter>().flowerCanvasText.GetComponent<TMP_Text>().text, out count);
+
+        if (count >= CountTreshold)
+            gameObject.GetComponent<AudioSource>().clip = GoodEndingAudio;
+        else
+            gameObject.GetComponent<AudioSource>().clip = BadEndingAudio;
+        
+        gameObject.GetComponent<AudioSource>().Play(); 
+    }
+}
