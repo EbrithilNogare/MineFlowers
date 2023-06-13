79bd010be123499cc3c834eda84af9cd56bd97c2
David Napravnik <d@nogare.cz>
2023-06-13 18:04:44 +0200

    added changeable by time on third mechanic

--- Assets/Scripts/Mechanics/ImaginationLock/ImaginationUnlocker.cs
+++ Assets/Scripts/Mechanics/ImaginationLock/ImaginationUnlocker.cs
@@ -5,53 +5,126 @@ using UnityEngine.Events;
 
 public class ImaginationUnlocker : MonoBehaviour
 {
-    #region [SerializeField] public float imaginationUnlockedAt
+    [Header("--- Outer refs ---")]
+    
+    #region public GameObject imaginationLevel
 
-    /// <summary>
-    /// TODO
-    /// </summary>
-    [Tooltip("TODO")]
+    public GameObject imaginationLevel;
+
+    #endregion
+
+    [Header("--- Inner refs ---")]
+
+    #region public float imaginationUnlockedAt
+
     [Range(0, 1f)]
     public float imaginationUnlockedAt;
 
     #endregion
 
-    #region [SerializeField] public GameObject unlockedVariant { get; }
+    #region public GameObject lockedVariant
 
-    [SerializeField]
-    [Tooltip("Set unlocked variant.")]
-    private GameObject _unlockedVariant;
-
-    /// <summary>
-    /// Set unlocked variant.
-    /// </summary>
-    public GameObject unlockedVariant => _unlockedVariant;
+    public GameObject lockedVariant;
 
     #endregion
 
-    #region [SerializeField] public GameObject imaginationLevel { get; }
+    #region public GameObject unlockedVariant
 
-    [SerializeField]
-    private GameObject _imaginationLevel;
-
-    /// <summary>
-    /// TODO
-    /// </summary>
-    public GameObject imaginationLevel => _imaginationLevel;
+    public GameObject unlockedVariant;
 
     #endregion
 
-    private float imagination;
+    #region public float timeToChange
 
-    private void OnTriggerStay(Collider other)
+    public float timeToChange;
+
+    #endregion
+
+    #region public Material[] materialsToChange
+
+    public Material[] materialsToChange;
+
+    #endregion
+
+
+    private RealityAwareness imagination;
+    private float timeProgress = 0;
+    private bool isChanging = false;
+
+    private void Start()
     {
-        imagination = imaginationLevel.GetComponent<RealityAwareness>().awareness;
-        if (imagination >= imaginationUnlockedAt)
+        imagination = imaginationLevel.GetComponent<RealityAwareness>();
+
+        for (int i = 0; i < materialsToChange.Length; i++)
         {
-            gameObject.SetActive(false);
-            unlockedVariant.SetActive(true);
+            Material oldMaterial = materialsToChange[i];
+            materialsToChange[i] = new Material(oldMaterial);
+            materialsToChange[i].name += " copy";
+
+            TraverseAndChangeMaterial(transform, oldMaterial, materialsToChange[i]);
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (imagination.awareness >= imaginationUnlockedAt && !isChanging)
+        {
+            changingStart();
+        }
+
+        if (isChanging)
+        {
+            changingStep();
+        }
+    }
+
+    private void changingStart()
+    {
+        isChanging = true;
+        unlockedVariant.SetActive(true);
+    }
+
+    private void changingStep()
+    {
+        timeProgress += Time.deltaTime / timeToChange;
+        
+        foreach (var material in materialsToChange)
+        {
+            material.SetFloat("_Progress", timeProgress);
+        }
+
+        if (timeProgress >= 1) { 
+            changingEnd();
+        }
+    }
+
+    private void changingEnd()
+    {
+        isChanging = false;
+        lockedVariant.SetActive(false);
+    }
+
+    void TraverseAndChangeMaterial(Transform parent, Material oldMaterial, Material newMaterial)
+    {
+        MeshRenderer renderer = parent.GetComponent<MeshRenderer>();
+
+        if (
+            renderer != null
+            && (
+                renderer.sharedMaterial.name == oldMaterial.name + " (Instance)"
+                || renderer.sharedMaterial.name == oldMaterial.name
+            )
+            && renderer.sharedMaterial.shader.name == oldMaterial.shader.name
+            )
+        {
+            renderer.sharedMaterial = newMaterial;
+            
+        }
+
+        foreach (Transform child in parent)
+        {
+            TraverseAndChangeMaterial(child, oldMaterial, newMaterial);
+        }
+    }
 
 }
